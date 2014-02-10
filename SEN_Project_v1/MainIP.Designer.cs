2cdfb221a213f259816229ff4cdb16e5d76217b1
AnujKosambi <anujkosambi@gmail.com>
2014-02-10 23:12:13 +0530

    Initial Base

---
+++ SEN_Project_v1/MainIP.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace SEN_Project_v1
+{
+    partial class MainIP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startChatButton = new System.Windows.Forms.Button();
+            this.statusText = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.sendBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startChatButton
+            // 
+            this.startChatButton.Location = new System.Drawing.Point(12, 287);
+            this.startChatButton.Name = "startChatButton";
+            this.startChatButton.Size = new System.Drawing.Size(316, 23);
+            this.startChatButton.TabIndex = 0;
+            this.startChatButton.Text = "Chat";
+            this.startChatButton.UseVisualStyleBackColor = true;
+            // 
+            // statusText
+            // 
+            this.statusText.Location = new System.Drawing.Point(13, 13);
+            this.statusText.Name = "statusText";
+            this.statusText.Size = new System.Drawing.Size(315, 20);
+            this.statusText.TabIndex = 1;
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.5F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 72.5F));
+            this.tableLayoutPanel.Location = new System.Drawing.Point(13, 130);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 2;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(315, 151);
+            this.tableLayoutPanel.TabIndex = 3;
+            // 
+            // sendBox
+            // 
+            this.sendBox.Location = new System.Drawing.Point(13, 39);
+            this.sendBox.Name = "sendBox";
+            this.sendBox.Size = new System.Drawing.Size(315, 20);
+            this.sendBox.TabIndex = 4;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(12, 65);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(316, 23);
+            this.sendButton.TabIndex = 5;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // MainIP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 322);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.sendBox);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Controls.Add(this.statusText);
+            this.Controls.Add(this.startChatButton);
+            this.Name = "MainIP";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IPMsg";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainIP_FormClosing);
+            this.Load += new System.EventHandler(this.MainIP_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startChatButton;
+        private System.Windows.Forms.TextBox statusText;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.TextBox sendBox;
+        private System.Windows.Forms.Button sendButton;
+    }
+}
+
---
+++ SEN_Project_v1/MainIP.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WebCam_Capture;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace SEN_Project_v1
+{
+    public partial class MainIP : Form
+    {
+        #region INIT
+        public static UdpClient memberRetrive = null;
+        static int PORT = 1716;
+        IPEndPoint BROADCAST_SENDING = new IPEndPoint(IPAddress.Parse("255.255.255.255"), PORT);
+        IPEndPoint BROADCAST_RECIVEING= new IPEndPoint(IPAddress.Any, PORT);
+        public static VideoChatting _videoChatting;
+        Thread tBroadCast_r=null;
+        #endregion
+
+        public MainIP()
+        {
+            memberRetrive = new UdpClient(PORT);
+      
+            InitializeComponent();
+ 
+        }
+        private void MainIP_Load(object sender, EventArgs e)
+        {
+            tBroadCast_r = new Thread(new ThreadStart(vrecving_proc));
+            tBroadCast_r.Start();
+        }
+        private void vrecving_proc()
+        {
+            
+            while(true)
+            {
+                BeginInvoke((Action)(()=>{statusText.Text = "Ready For Reciving";
+                System.Diagnostics.Debug.WriteLine(statusText.Text);
+                }));
+
+
+                byte[] data;
+                IPEndPoint receving= new IPEndPoint(IPAddress.Any, PORT);
+                data = memberRetrive.Receive(ref receving);
+                string stringData = Encoding.ASCII.GetString(data, 0, data.Length);
+                System.Diagnostics.Debug.WriteLine("received:"+stringData+" "+receving.Address);
+                BeginInvoke((Action)(() =>
+                {
+                    Label l = new Label();
+                    l.Text = receving.Address.ToString();
+                    tableLayoutPanel.Controls.Add(l);
+                }));
+
+            }
+        }
+        private void MainIP_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            AbortThread(tBroadCast_r);
+            if (memberRetrive != null) memberRetrive.Close();
+        }
+        private void AbortThread(Thread t){
+            
+            if (t!= null && t.IsAlive)
+                t.Abort();
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+}
